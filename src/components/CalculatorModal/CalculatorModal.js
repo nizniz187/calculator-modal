e32d3c41fad63612eca4dd0e0b5080f4d4d3c2d0
Niz <nizniz187@gmail.com>
2020-05-06 20:37:22 +0800

    Use immutable map for command text. Add basic command functions.

--- src/components/CalculatorModal/CalculatorModal.js
+++ src/components/CalculatorModal/CalculatorModal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from './CalculatorButton.js';
+import { Map } from 'immutable';
 import './CalculatorModal.css';
 
 class CalculatorModal extends React.Component {
@@ -9,30 +10,47 @@ class CalculatorModal extends React.Component {
         <div className="calc-modal-ctn">
           <div className="calc-modal-input">{this.props.input}</div>
           <div className="calc-modal-ctrl">            
-            <Button color="gray" text="AC" clickHandler={this.props.resetHandler} />
-            <Button color="gray" text="+/-" />
-            <Button color="gray" text="%" />
-            <Button color="blueSky" text="÷" />
+            <Button color="gray" text="AC"
+              clickHandler={this.props.resetHandler} />
+            <Button color="gray" text="+/-"
+              clickHandler={this.props.convertSignHandler} />
+            <Button color="gray" text="%"
+              clickHandler={this.props.convertPercentageHandler} />
+            <Button color="blueSky" text={COMMAND_TEXT.get('DIVIDE')}
+              clickHandler={this.props.showResultHandler} />
             <Button color="grayDark" text="7" clickHandler={this.props.addInputHandler} />
             <Button color="grayDark" text="8" clickHandler={this.props.addInputHandler} />
             <Button color="grayDark" text="9" clickHandler={this.props.addInputHandler} />
-            <Button color="blueSky" text="&times;" />
+            <Button color="blueSky" text={COMMAND_TEXT.get('MULTIPLY')}
+              clickHandler={this.props.showResultHandler} />
             <Button color="grayDark" text="4" clickHandler={this.props.addInputHandler} />
             <Button color="grayDark" text="5" clickHandler={this.props.addInputHandler} />
             <Button color="grayDark" text="6" clickHandler={this.props.addInputHandler} />
-            <Button color="blueSky" text="-" />
+            <Button color="blueSky" text={COMMAND_TEXT.get('SUBTRACT')}
+              clickHandler={this.props.showResultHandler} />
             <Button color="grayDark" text="1" clickHandler={this.props.addInputHandler} />
             <Button color="grayDark" text="2" clickHandler={this.props.addInputHandler} />
             <Button color="grayDark" text="3" clickHandler={this.props.addInputHandler} />
-            <Button color="blueSky" text="+" />
-            <Button color="grayDark" text="0" size="2" clickHandler={this.props.addInputHandler} />
+            <Button color="blueSky" text={COMMAND_TEXT.get('ADD')}
+              clickHandler={this.props.showResultHandler} />
+            <Button color="grayDark" text="0" size="2"
+              clickHandler={this.props.addInputHandler} />
             <Button color="grayDark" text="." clickHandler={this.props.addInputHandler} />
-            <Button color="blueSky" text="=" />
+            <Button color="blueSky" text={COMMAND_TEXT.get('EQUAL')}
+              clickHandler={this.props.showResultHandler} />
           </div>
         </div>
       </div>
     );
   }
 }
+
+export const COMMAND_TEXT = Map({
+  ADD: '+',
+  SUBTRACT: '-',
+  MULTIPLY: '×',
+  DIVIDE: '÷',
+  EQUAL: '='
+});
 
 export default CalculatorModal;
--- src/containers/CalculatorModalContainer/CalculatorModalContainer.js
+++ src/containers/CalculatorModalContainer/CalculatorModalContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CalculatorModal from 'components/CalculatorModal/CalculatorModal.js';
+import CalculatorModal, { COMMAND_TEXT } from 'components/CalculatorModal/CalculatorModal.js';
 import './CalculatorModalContainer.css';
 
 class CalculatorModalContainer extends React.Component {
@@ -7,10 +7,11 @@ class CalculatorModalContainer extends React.Component {
     super(props);
     this.state = {
       isCalculatorShow: true,
+      newInput: true,
       input: '0',
-      result: 0,
-      arithmetic: null
-    }
+      result: '0',
+      command: null
+    };
   }
   render() {
     if(this.state.isCalculatorShow === true) {
@@ -23,7 +24,6 @@ class CalculatorModalContainer extends React.Component {
             resetHandler={this.reset}
             convertSignHandler={this.convertSign}
             convertPercentageHandler={this.convertPercentage}
-            changeArithmeticHandler={this.changeArithmetic}
             showResultHandler={this.showResult}
           />
         </div>
@@ -35,6 +35,11 @@ class CalculatorModalContainer extends React.Component {
 
   addInput = (input = '') => {
     this.setState(state => {
+      if(state.newInput === true) { 
+        if(input === '.') { return { input: '0.', newInput: false }; }
+        else { return { input: input, newInput: false }; }
+      }
+
       if(state.input.length >= 7) { return state; }
       if(input === '.' && state.input.includes('.')) { return state; }
       if(state.input === '0' && input !== '.') { return { input: input }; }
@@ -44,31 +49,72 @@ class CalculatorModalContainer extends React.Component {
   };
   reset = () => {
     this.setState(() => {
-      return { 
-        input: '0', result: 0
+      return {
+        newInput: true,
+        input: '0',
+        result: '0',
+        command: null
       };
     });
   };
   convertSign = () => {
+    this.setState(state => {
+      if(state.input === '0') { return state; }
 
+      if(state.input.includes('-')) {
+        return { input: state.input.replace('-', '') };
+      } else {
+        return { input: '-' + state.input };
+      }
+    });
   };
   convertPercentage = () => {
-
+    this.setState(state => {
+      if(state.command === null) { return { input: '0' }; }
+    });
   };
-  changeArithmetic = () => {
-
+  showResult = command => {
+    this.setState(state => {
+      let input = this.executeArithmetic(state.command, state.result, state.input);
+      return { input: `${input}`, result: `${input}`, command, newInput: true };
+    });
   };
-  showResult = () => {
 
-  };
   hideCalculator = e => {
     if(e.target.className === MASK_CLASS_NAME) {
       this.setState(() => ({ isCalculatorShow: false }));
     }
-  }
+  };
   showCalculator = () => {
     this.setState(() => ({ isCalculatorShow: true }));
   };
+
+  executeArithmetic(arithmetic, input1, input2) {
+    input1 = Number(input1);
+    input2 = Number(input2);
+    switch(arithmetic) {
+      case COMMAND_TEXT.get('ADD'):
+        return input1 + input2;
+      case COMMAND_TEXT.get('SUBTRACT'):
+        return input1 - input2;
+      case COMMAND_TEXT.get('MULTIPLY'):
+        return input1 * input2;
+      case COMMAND_TEXT.get('DIVIDE'):
+        return input1 / input2;
+      default:
+        return input2;
+    }
+  }
+  isCommandArithmetic(command) {
+    switch(command) {
+      case COMMAND_TEXT.get('ADD'):
+      case COMMAND_TEXT.get('SUBTRACT'):
+      case COMMAND_TEXT.get('MULTIPLY'):
+      case COMMAND_TEXT.get('DIVIDE'):
+        return true;
+    }
+    return false;
+  }
 }
 
 const MASK_CLASS_NAME = 'calculator-mask';
